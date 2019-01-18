4a702f2e590d02fdb6c0a6cad624d77170ff1be2
zep1994 <snaxbags@protonmail.com>
2019-01-18 11:38:45 -0600

    Solution

--- 1/Challenge1.rb
+++ 1/Challenge1.rb
@@ -13,6 +13,11 @@ string_array = [
 ]
 
 def string_parser string_array
+  final_array = []
+  string_array.each do |str|
+      final_array << str.scan(/\d+/).last.to_i
+  end
+  final_array
 end
 
 describe "String Parser" do
