03d857d5f8cf1a7d8685806bed6fff132aa60af6
zep1994 <snaxbags@protonmail.com>
2019-01-18 14:10:50 -0600

    Solution

--- 5/max.rb
+++ 5/max.rb
@@ -1,9 +1,28 @@
 require 'rspec'
+require 'benchmark'
+
+arr = Array.new(1_000) { rand 1000 }
 
 def new_max array_of_elements
-    #array_of_elements.max
-    array_of_elements.sort.last
+    max = 0 
+
+    array_of_elements.each do |i|
+        if i > max
+            max = i
+        end
+    end
+    max   
+end 
+
+Benchmark.bm(10) do |x|
+    x.report('Each: ') { new_max arr }
+    x.report('Sorted: ') { arr.sort.last }
 end
+
+# def new_max array_of_elements
+#     #array_of_elements.max
+#     #array_of_elements.sort.last
+# end
 
 describe 'New Max method' do
   it 'returns the max value from an array efficiently' do
