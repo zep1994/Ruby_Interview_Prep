d83ab0943bf8113977d4178d447bd3baf4996e34
zep1994 <snaxbags@protonmail.com>
2019-01-18 16:48:53 -0600

    Recursive solution

--- 7_palindrome/palindrome.rb
+++ 7_palindrome/palindrome.rb
@@ -1,8 +1,21 @@
 require 'rspec'
 
-def palindrome? word
-   word.downcase == word.downcase.reverse
-end
+# def palindrome? word
+#    word.downcase == word.downcase.reverse
+# end
+
+def palindrome?(string)
+    str = string.downcase
+    if str.length == 1 || str.length == 0
+      true
+    else
+      if str[0] == str[-1]
+        palindrome?(str[1..-2])
+      else
+        false
+      end
+    end
+  end
 
 describe 'Check if a word is a palindrome' do
   it 'returns true if the word is a palindrome, false if not' do
