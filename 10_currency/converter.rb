d1b6cfea57e54cb73d5ab1d155e3f3d02a6c74b4
zep1994 <snaxbags@protonmail.com>
2019-01-18 18:06:23 -0600

    Solution

---
+++ 10_currency/converter.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+
+# ¥
+# £
+# $
+
+# money = 10.2
+# "$%.2f" % money
+
+def currency_converter amount, location
+    case location 
+    when 'US' then "$%.2f" % amount
+    when 'Japan' then "¥%.0f" % amount
+    when 'UK' then ("£%.2f" % amount).gsub('.', ',')
+    end
+end
+
+describe 'Currency converter' do
+  it 'can properly format currency for US, Japan, and UK' do
+    expect(currency_converter 5000, 'US').to eq('$5000.00')
+    expect(currency_converter 5000, 'Japan').to eq('¥5000')
+    expect(currency_converter 5000, 'UK').to eq('£5000,00')
+  end
+end
